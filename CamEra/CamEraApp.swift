f2655ab33b959b95e8f00bb28820042823b11a03
bbetperf <tech@bbetperf.com>
2024-10-03 10:28:12 -0700

    Initial Commit

---
+++ CamEra/CamEraApp.swift
@@ -0,0 +1,17 @@
+//
+//  CamEraApp.swift
+//  CamEra
+//
+//  Created by Maksim on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CamEraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CamEraTests/CamEraTests.swift
@@ -0,0 +1,17 @@
+//
+//  CamEraTests.swift
+//  CamEraTests
+//
+//  Created by Maksim on 10/3/24.
+//
+
+import Testing
+@testable import CamEra
+
+struct CamEraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
